7fc3359d9abaabdcd68aaa39bd126a0f752d5722
Amos932 <63639642+Amos932@users.noreply.github.com>
2020-04-21 16:31:33 +0200

    Update toevoegenproduct.php

--- toevoegenproduct.php
+++ toevoegenproduct.php
@@ -1,50 +1,69 @@
-<!Doctype HTML>
-<html lang="nl">
-    <head>
-        <title>Amos Middelkoop</title>
-        <meta charset="UTF-8">
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <link rel="stylesheet" href="CSS/styles.css">
-    </head>
-    <body>
-<form method="POST" action="inlogvalidatie.php"> 
+<?php
 
-    <label for="Titel">Titel voorwerp</label>
+session_start();
+
+$title = "toevoegenproduct";
+
+require_once('includes/database.php');
+
+require_once('includes/functions.php');
+
+require_once('includes/header.php');
+
+checkLogin();
+
+?>
+
+<form method="POST"> 
+
+    <label for="Titel">Titel voorwerp: </label>
     <input type="text" name="title_product" id="Titel" required> <br>
 
-    <label for="Beschrijving">Beschrijving</label>
+    <label for="Beschrijving">Beschrijving: </label>
     <input type="text" name="description_product" id="Beschrijving" required><br>
 
-    <label for="Startprijs">Startprijs</label>
+    <label for="Rubriek">Rubriek: </label>
+    <select id="Rubriek" name="categorie" required>
+        <option value="Auto's, boten en motoren">Autos, boten en motoren</option>
+        <option value="Baby">Baby</option>
+        <option value="Muziek en Instrumenten">Muziek en instrumenten</option>
+        <option value="Elektronica">Elektronica</option>
+        <option value="Mode">Mode</option>
+    </select><br>
+    
+	<label for=img>Select image: </label> 
+	<input type=file id=img name=img accept=image/*> <br> 
+
+    <label for="Startprijs">Startprijs: </label>
     <input type="number" name="starting_price_product" id="Startprijs" required><br>
 
-    <label for="Betalingswijze">Betalingswijze</label>
+    <label for="Betalingswijze">Betalingswijze: </label>
     <select id="Betalingswijze" name="payment_method_product" required>
         <option value="Contact">Contact</option>
         <option value="Bank">Bank</option>
         <option value="Anders">Anders</option>
     </select> <br>
 
-    <label for="Betalingsinstructies">Betalingsinstructies</label>
+    <label for="Betalingsinstructies">Betalingsinstructies: </label>
     <input type="text" name="payment_instructions" id="Betalingsinstructies"><br>
 
-    <label for="Looptijd">Looptijd</label>
+    <label for="Looptijd">Looptijd: </label>
     <select id="Looptijd" name="duration" required>
         <option value="1">1</option>
         <option value="3">3</option>
         <option value="5">5</option>
-        <option value="7">7</option>
+        <option value="7" selected="selected">7</option>
         <option value="10">10</option>
     </select><br>
 
-    <label for="Verzendinstructies">Verzendinstructies</label>
+    <label for="Verzendinstructies">Verzendinstructies: </label>
     <input type="text" name="payment_instructions" id="Verzendinstructies"><br>
 
-    <input type="submit" name="submit" value="Submit">
-
+    <input type="submit" name="submit" value="Verzenden">
 </form>  
 
-</body>
+<?php
 
-</html>
+require_once('includes/footer.php');
 
+?>
