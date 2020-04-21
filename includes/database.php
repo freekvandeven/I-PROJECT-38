9587529418f46b587b97e07fc500ff5560bcf529
Freek van de Ven <freek@freekvandeven.nl>
2020-04-21 13:57:19 +0200

    add register request handling

--- includes/database.php
+++ includes/database.php
@@ -24,3 +24,22 @@ function getUser($username){
     $user = $data->fetch(PDO::FETCH_ASSOC);
     return $user;
 }
+
+function insertUser($user){
+    global $dbh;
+    $data = $dbh->prepare('INSERT INTO Gebruiker (Gebruikersnaam, Voornaam, Achternaam, Adresregel_1, Adresregel_2, 
+                       Postcode, Plaatsnaam, Land, Geboortedag, Mailbox, Wachtwoord, Vraag, Antwoordtekst, Verkoper, action) VALUES (:Gebruikersnaam,:Voornaam,:Achternaam,:Adresregel_1,
+                                                                                                                                     :Adresregel_2  ,:Postcode,:Plaatsnaam,:Land,:Geboortedag,
+                                                                                                                                     :Mailbox,:Wachtwoord,:Vraag,:Antwoordtekst,:Verkoper,:Action)');
+    var_dump($user);
+    $data->execute($user);
+    //array_unshift($array, $id)
+    /*
+    $sql = sprintf('SELECT * FROM user WHERE name LIKE :name %s %s',
+        !empty($_GET['city'])   ? 'AND city   = :city'   : null,
+        !empty($_GET['gender']) ? 'AND gender = :gender' : null);
+    if (!empty($_GET['city'])) {
+        $stmt->bindParam(':city', '%'.$_GET['city'].'%', PDO::PARAM_STR);
+    }
+    */
+}
