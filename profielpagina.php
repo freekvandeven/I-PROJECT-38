98a1240b7d9fae9e204adb737b40ffd435f3bbf4
Amos932 <63639642+Amos932@users.noreply.github.com>
2020-04-22 15:04:05 +0200

    Add files via upload

--- profielpagina.php
+++ profielpagina.php
@@ -1,38 +1,30 @@
 <?php
-
 session_start();
+checkLogin();
 
 $title = "profielpagina";
 
-require_once('includes/database.php');
+$edit = false;
 
+require_once('includes/database.php');
 require_once('includes/functions.php');
 
-require_once('includes/header.php');
+if(isset($_POST['Edit'])){
+    $edit = true;
+} 
 
-$profile_data = getUser($_SESSION['name']);
+if($edit === false){
+    require_once('C:\xampp\htdocs/viewProfiel.php');
+    if(isset($_POST['Verzenden'])){
+       //update database
+    }
+} else {
+    require_once('C:\xampp\htdocs/updateprofiel.php');
+    $edit = false;
+}
 
 ?>
-<h2>Profielpagina</h2>
-
-<h4>Persoonlijke gegevens:</h4><br>
-<p>Gebruikersnaam: <?=$profile_data['Gebruikersnaam']?></p><br>
-<p>Voornaam: <?=$profile_data['Voornaam']?></p><br>
-<p>Achternaam:<?=$profile_data['Achternaam']?></p><br>
-<p>Adres 1: <?=$profile_data['Adresregel_1']?></p><br>
-<p>Adres 2: <?=$profile_data['Adresregel_2']?></p><br>
-<p>Postcode: <?=$profile_data['Postcode']?></p><br>
-<p>Plaats: <?=$profile_data['Plaatsnaam']?></p><br>
-<p>Land: <?=$profile_data['Land']?></p><br>
-<p>Geboortedatum: <?=$profile_data['Geboortedag']?></p><br>
-<p>Email: <?=$profile_data['Mailbox']?></p><br>
-<p>Wachtwoord: <?=$profile_data['Wachtwoord']?></p><br>
-<p>Vraag: <?=$profile_data['Vraag']?></p><br>
-<p>Verkoper: <?=$profile_data['Verkoper']?></p><br>
-<p>Action: <?=$profile_data['Action']?></p><br>
 
 <?php
-
-require_once('includes/footer.php');
-
+require_once('includes/header.php');
 ?>
