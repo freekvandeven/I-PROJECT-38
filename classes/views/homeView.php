be11cd26dd2bd474d9f32ae44f23e75766366adb
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-04-22 12:25:26 +0200

    1. css van inlogpagina gemaakt 2. loginView.php anders gemaakt zodat het past met de bootstrapstandaarden 3. een regel in de homepage verbeterd

--- classes/views/homeView.php
+++ classes/views/homeView.php
@@ -2,7 +2,7 @@
     <!-- Welkom tekst: -->
     <div class="jumbotron">
         <h2 class="display-5">Welkom op EenmaalAndermaal</h2>
-        <p class="lead">EenmaalAndermaal is een online veilingsite waar u uw producten kunt aanbieden en u  kunt bieden op andermans producten.</p>
+        <p class="lead">EenmaalAndermaal is een online veilingsite waar u uw producten kunt aanbieden en kunt bieden op andermans producten.</p>
     </div>
 
     <!-- Slideshow: -->
--- classes/views/loginView.php
+++ classes/views/loginView.php
@@ -1,9 +1,33 @@
 <main>
-    <form method="POST" action="">
-        <label for="gebruikersnaam">Gebruikersnaam</label>
-        <input type="text" name="username" id="gebruikersnaam" required> <br>
-        <label for="wachtwoord"> Wachtwoord</label>
-        <input type="password" name="password"  id="wachtwoord" required><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="loginForm" action="" method="post">
+                            <h2 class="text-center">Inloggen</h2>
+                            <div class="form-group" style="margin-top: 25px;">
+                                <label for="gebruikersnaam">Gebruikersnaam:</label><br>
+                                <input type="text" name="gebruikersnaam" id="gebruikersnaam" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="wachtwoord">Wachtwoord:</label><br>
+                                <input type="password" name="wachtwoord" id="wachtwoord" class="form-control" required>
+                            </div>
+                            <div class="form-group text-left">
+                                <a href="#">Wachtwoord vergeten?</a>
+                            </div>
+                            <div class="form-group text-left">
+                                <a href="#">Nog geen account?</a>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" name="submit" value="Inloggen">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
