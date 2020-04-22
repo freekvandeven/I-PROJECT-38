78051c793e81af9ccd0269f5c2593f84a4a7aace
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-04-22 15:31:50 +0200

    1. css van inlogpagina en registerpagina gemaakt. 2. login.php en register.php aangepast voor de css.

--- classes/views/loginView.php
+++ classes/views/loginView.php
@@ -1,6 +1,5 @@
 <main>
     <div id="login">
-        <h3 class="text-center text-white pt-5">Login form</h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
@@ -9,17 +8,17 @@
                             <h2 class="text-center">Inloggen</h2>
                             <div class="form-group" style="margin-top: 25px;">
                                 <label for="gebruikersnaam">Gebruikersnaam:</label><br>
-                                <input type="text" name="gebruikersnaam" id="gebruikersnaam" class="form-control" required>
+                                <input type="text" name="gebruikersnaam" id="gebruikersnaam" class="form-control" placeholder="Uw gebruikersnaam" required>
                             </div>
                             <div class="form-group">
                                 <label for="wachtwoord">Wachtwoord:</label><br>
-                                <input type="password" name="wachtwoord" id="wachtwoord" class="form-control" required>
+                                <input type="password" name="wachtwoord" id="wachtwoord" class="form-control" placeholder="Uw wachtwoord" required>
                             </div>
                             <div class="form-group text-left">
                                 <a href="#">Wachtwoord vergeten?</a>
                             </div>
                             <div class="form-group text-left">
-                                <a href="#">Nog geen account?</a>
+                                <a href="register.php">Nog geen account?</a>
                             </div>
                             <div class="form-group text-center">
                                 <input type="submit" name="submit" value="Inloggen">
