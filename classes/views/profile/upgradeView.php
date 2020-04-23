1689f6b759bd89221ed4cf1e8f75c1dd08e4fa50
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-04-23 11:42:18 +0200

    css van de upgradepagina bijgevoegd.

--- classes/views/profile/upgradeView.php
+++ classes/views/profile/upgradeView.php
@@ -2,7 +2,7 @@
 $profile_data = getUser($_SESSION['name']);
 ?>
 <main>
-    <form id="login-form" class="registerForm" action="" method="post">
+    <form id="login-form" class="verkoperForm" action="" method="post">
         <h2 class="text-center">Wordt een verkoper</h2>
         <!-- VERKOPER GEGEVENS-->
         <div class="container">
@@ -11,30 +11,34 @@ $profile_data = getUser($_SESSION['name']);
                 <!-- BANK -->
                 <div class="form-group col-md-6">
                     <label for="bank">Bank</label>
-                    <input type="text" class="form-control" id="bank" placeholder="Uw bank">
+                    <input type="text" class="form-control" name="" id="bank" placeholder="Uw bank">
                 </div>
                 <!-- BANKREKENING -->
                 <div class="form-group col-md-6">
                     <label for="bankrekening">Bankrekening</label>
-                    <input type="text" class="form-control" id="bankrekening" placeholder="Uw bankrekening">
+                    <input type="text" class="form-control" name="" id="bankrekening" placeholder="Uw bankrekening">
                 </div>
                 <!-- CONTROLEOPTIE -->
                 <div class="form-group col-md-6">
                     <label for="controlenummer">ControleOptie</label>
-                    <input type="text" class="form-control" id="controlenummer" placeholder="Uw controlenummer">
+                    <input type="text" class="form-control" name="" id="controlenummer" placeholder="Uw controlenummer">
                 </div>
                 <!-- CREDITCARD -->
                 <div class="form-group col-md-6">
                     <label for="creditcard">Creditcard</label>
-                    <input type="text" class="form-control" id="creditcard" placeholder="Uw creditcard">
+                    <input type="text" class="form-control" name="" id="creditcard" placeholder="Uw creditcard">
                 </div>
 
             </div>
         </div>
+        <!-- GA-TERUG-KNOP -->
+        <div class="form-group text-center">
+            <a href="profile.php">Ga terug</a><br>
+        </div>
+
         <!-- SUBMIT-KNOP -->
         <div class="form-group text-center">
-            <input type="submit" name="action" value="upgrade">
+            <input type="submit" name="action" value="Bevestigen">
         </div>
     </form>
-    <a href="profile.php">Ga terug</a>
 </main>
