2ef86d5e861272a24a406e5dd858387581a0ab48
Amos932 <63639642+Amos932@users.noreply.github.com>
2020-04-24 13:43:44 +0200

    Update catalogusproducten.php

--- catalogusproducten.php
+++ catalogusproducten.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-checkLogin();
 
 $title = "catalogusproducten";
 
@@ -8,20 +7,17 @@ require_once('includes/database.php');
 require_once('includes/functions.php');
 require_once('includes/header.php');
 
-// ophalen van producten: $producten = getItems();
+//ophalen van producten: $producten = getItems();
 
-$index = 0;
-foreach($producten as $item){
-    $product[$index] = "<h2>" . $item['titel'] . "</h2> <br>";
-    $product[$index] .= "<p>" . $item['prijs'] . "</p> <br>";
-    // hoe moet je elk bestand checken?
-    if(file_exists('EenmaalAndermaalDEF\Images\hond.jpg')){
-        $product[$index] .= "<img src=''EenmaalAndermaalDEF\Images\hond.jpg' alt='Foto van product'>";
-    } else {
-        $product[$index] .= "<img src='" . $item['afbeelding'] . ".png' alt='Fotooooooo van product'";
-    }
-    echo $product[$index];
-}
+foreach($producten as $item) : ?>
+<h2>Titel</h2>
+<p>Prijs</p>
+<?php if(file_exists('upload\items\ '. $item['afbeelding'])) : ?>
+    <img src='upload\items\' . <?=$item['afbeelding']?> alt="Foto van product">
+    <?php else: ?>
+    <img src='upload\items\defaultAfbeelding.png' alt='Default foto'>
+<?php endif; ?>
+<?php endforeach; 
 
-require_once('includes/footer.php');
+require_once('includes/footer.php');?>
 
