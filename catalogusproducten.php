304ae8350b5df0859d6d40207b4ef65a0d750c1f
Amos932 <63639642+Amos932@users.noreply.github.com>
2020-04-24 11:59:12 +0200

    Add files via upload

--- catalogusproducten.php
+++ catalogusproducten.php
@@ -1,36 +1,33 @@
 <?php
+session_start();
+checkLogin();
 
-$producten = array(2, 3, 5, 6 , 7 );
+$title = "catalogusproducten";
+
+require_once('includes/database.php');
+require_once('includes/functions.php');
+require_once('includes/header.php');
+
+// ophalen van producten: $producten = getItems();
 
 $index = 0;
-
 foreach($producten as $item){
-    $producten[$index] = "<h2>" . $item['titel'] . "</h2> <br>";
-    $producten[$index] .= "<p>" . $item['prijs'] . "</p> <br>";
-    if(file_exists('C:\xampp\htdocs\images\afbeelding')){
-        $producten[$index] .= "<img src='" . $item['afbeelding'] . ".png' alt='Foto van product'";
+    $product[$index] = "<h2>" . $item['titel'] . "</h2> <br>";
+    $product[$index] .= "<p>" . $item['prijs'] . "</p> <br>";
+    // hoe moet je elk bestand checken?
+    if(file_exists('EenmaalAndermaalDEF\Images\hond.jpg')){
+        $product[$index] .= "<img src='hond.jpg' alt='Foto van product'>";
     } else {
-        $producten[$index] .= "<img src='" . $item['afbeelding'] . ".png' alt='Foto van product'";
+        $product[$index] .= "<img src='" . $item['afbeelding'] . ".png' alt='Fotooooooo van product'";
     }
     $index++;
 }
 
 $max = $index;
 
-?>
+for($index = 0;  $index < $max; $index++){
+    echo $product[$index];
+}
 
-<!Doctype HTML>
-<html lang="nl">
-    <head>
-        <title>Amos Middelkoop</title>
-        <meta charset="UTF-8">
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <link rel="stylesheet" href="CSS/styles.css">
-    </head>
-    <body>
-    <?php 
-    for($index = 0;  $index < $max; $index++){
-        echo $producten[$index];
-    }
-    ?>
-    </body>
+require_once('includes/footer.php');
+
