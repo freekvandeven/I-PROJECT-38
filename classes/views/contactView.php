cde8925e7976ca3324351055b2c55d875e51b8d5
Freek van de Ven <freek@freekvandeven.nl>
2020-04-24 17:47:46 +0200

    add emtpy contact page

---
+++ classes/views/contactView.php
@@ -0,0 +1,7 @@
+<main>
+    <!-- Welkom tekst: -->
+    <div class="jumbotron">
+        <h2 class="display-5">Welkom op EenmaalAndermaal</h2>
+        <p class="lead">EenmaalAndermaal is een online veilingsite waar u uw producten kunt aanbieden en kunt bieden op andermans producten.</p>
+    </div>
+</main>
---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$title = "Contact Page";
+require_once('includes/functions.php');
+
+require_once('includes/header.php');
+
+require_once('classes/views/contactView.php');
+
+require_once('includes/footer.php');
