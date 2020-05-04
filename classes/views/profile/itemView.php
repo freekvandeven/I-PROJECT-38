f1e677399af1507199ca0dfc6293f5f8f6fcef6e
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-04 16:43:46 +0200

    Added Bootstrap table

--- classes/views/profile/itemView.php
+++ classes/views/profile/itemView.php
@@ -4,25 +4,29 @@
 ?>
 
 <main>
-    <h2>Lijst van items</h2>
-    <table>
-        <thead>
-            <tr>
-                <td>Link:</td>
-                <?php foreach($displayedItems as $key){
-                    echo "<td>$key</td>";
-                } ?>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach($items as $item): ?>
-        <tr>
-            <td><a href="item.php?code=<?=$item['Voorwerpnummer']?>">Item <?=$item["Voorwerpnummer"]?></a></td>
-            <?php foreach($displayedItems as $itemName): ?>
-                <td><?=$item[$itemName]?></td>
-            <?php endforeach; ?>
-        </tr>
-        <?php endforeach;?>
-        </tbody>
-    </table>
+    <div class="mijnVeilingenBox">
+        <h2 class="text-center">Mijn veilingen</h2>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">Link</th>
+                    <?php foreach($displayedItems as $key){
+                        echo "<th>$key</th>";
+                    } ?>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach($items as $item): ?>
+                    <tr>
+                        <td><a href="item.php?code=<?=$item['Voorwerpnummer']?>">Voorwerp <?=$item["Voorwerpnummer"]?></a></td>
+                        <?php foreach($displayedItems as $itemName): ?>
+                            <td><?=$item[$itemName]?></td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach;?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </main>
