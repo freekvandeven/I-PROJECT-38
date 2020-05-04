052a2d4f3000f156eb6fcbf095df048e92b8b9f9
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-04 13:20:02 +0200

    addProductView.php HTML + CSS

--- classes/views/addProductView.php
+++ classes/views/addProductView.php
@@ -1,49 +1,94 @@
 <main>
-    <form method="POST" enctype="multipart/form-data">
+    <form id="login-form" class="addProductForm" action="" method="post" enctype="multipart/form-data">
+        <h2 class="text-center">Product verkopen</h2>
+        <div class="container">
+            <!-- PRODUCTGEGEVENS -->
+            <h5>Productgegevens</h5>
+            <div class="form-row">
+                <!-- TITEL -->
+                <div class="form-group col-md-6">
+                    <label for="bank">Titel</label>
+                    <input type="text" class="form-control" name="bank" id="bank" placeholder="Uw product" autofocus required>
+                </div>
+                <!-- RUBRIEK -->
+                <div class="form-group col-md-6">
+                    <label for="rubriek">Rubriek</label>
+                    <select  class="form-control" id="rubriek" name="categorie" required>
+                        <option value="Auto's, boten en motoren">Autos, boten en motoren</option>
+                        <option value="Baby">Baby</option>
+                        <option value="Muziek en Instrumenten">Muziek en instrumenten</option>
+                        <option value="Elektronica">Elektronica</option>
+                        <option value="Mode">Mode</option>
+                    </select>
+                </div>
+                <!-- BESCHRIJVING -->
+                <div class="form-group col-md-12">
+                    <label for="Beschrijving">Beschrijving</label>
+                    <input type="text" class="form-control" name="Beschrijving" id="Beschrijving" placeholder="Beschrijving" required>
+                </div>
 
-        <label for="Titel">Titel voorwerp: </label>
-        <input type="text" name="Titel" id="Titel" required> <br>
+                <!-- LOOPTIJD -->
+                <div class="form-group col-md-6">
+                    <label for="looptijd">Looptijd: </label>
+                    <select class="form-control" id="looptijd" name="looptijd" required>
+                        <option value="1">1</option>
+                        <option value="3">3</option>
+                        <option value="5" selected="selected">5</option>
+                        <option value="7">7</option>
+                        <option value="10">10</option>
+                    </select>
+                </div>
 
-        <label for="Beschrijving">Beschrijving: </label>
-        <input type="text" name="Beschrijving" id="Beschrijving" required><br>
+                <!-- FOTO UPLOADEN -->
+                <div class="form-group col-md-6">
+                    <label for="img">Foto uploaden</label>
+                    <input type="file" class="form-control" name="img" id="img" accept="image/*" required>
+                </div>
+            </div>
+        </div>
 
-        <label for="Rubriek">Rubriek: </label>
-        <select id="Rubriek" name="categorie" required>
-            <option value="Auto's, boten en motoren">Autos, boten en motoren</option>
-            <option value="Baby">Baby</option>
-            <option value="Muziek en Instrumenten">Muziek en instrumenten</option>
-            <option value="Elektronica">Elektronica</option>
-            <option value="Mode">Mode</option>
-        </select><br>
+        <div class="container">
+            <!-- BETALINGSGEGEVENS -->
+            <h5>Betalingsgegevens</h5>
+            <div class="form-row">
+                <!-- STARTPRIJS -->
+                <div class="form-group col-md-6">
+                    <label for="startprijs">Startprijs</label>
+                    <input type="number" class="form-control" name="startprijs" id="startprijs" required>
+                </div>
 
-        <label for=img>Select image: </label>
-        <input type=file id=img name=img accept=image/*> <br>
+                <!-- BETALINGSWIJZE -->
+                <div class="form-group col-md-6">
+                    <label for="betalingswijze">Betalingswijze</label>
+                    <select class="form-control" id="betalingswijze" name="betalingswijze" required>
+                        <option value="Contact">Contact</option>
+                        <option value="Bank">Bank</option>
+                        <option value="Anders">Anders</option>
+                    </select>
+                </div>
 
-        <label for="Startprijs">Startprijs: </label>
-        <input type="number" name="Startprijs" id="Startprijs" required><br>
+                <!-- BETALINGSINSTRUCTIES -->
+                <div class="form-group col-md-12">
+                    <label for="Betalingsinstructie">Betalingsinstructies: </label>
+                    <input type="text" class="form-control" name="Betalingsinstructie" id="Betalingsinstructie" required>
+                </div>
 
-        <label for="Betalingswijze">Betalingswijze: </label>
-        <select id="Betalingswijze" name="Betalingswijze" required>
-            <option value="Contact">Contact</option>
-            <option value="Bank">Bank</option>
-            <option value="Anders">Anders</option>
-        </select> <br>
+                <!-- VERZENDINSTRUCTIES -->
+                <div class="form-group col-md-12">
+                    <label for="Verzendinstructies">Verzendinstructies: </label>
+                    <input type="text" class="form-control" name="Verzendinstructies" id="Verzendinstructies" required>
+                </div>
+            </div>
+        </div>
 
-        <label for="Betalingsinstructie">Betalingsinstructie: </label> <!-- verander naar Betalingsinstructies met een S-->
-        <input type="text" name="Betalingsinstructie" id="Betalingsinstructie"><br>
+        <!-- GA-TERUG-KNOP -->
+        <div class="form-group text-center">
+            <a href="profile.php">Ga terug</a><br>
+        </div>
 
-        <label for="Looptijd">Looptijd: </label>
-        <select id="Looptijd" name="Looptijd" required>
-            <option value="1">1</option>
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="7" selected="selected">7</option>
-            <option value="10">10</option>
-        </select><br>
-
-        <label for="Verzendinstructies">Verzendinstructies: </label>
-        <input type="text" name="Verzendinstructies" id="Verzendinstructies"><br>
-
-        <input type="submit" name="submit" value="Verzenden">
+        <!-- SUBMIT-KNOP -->
+        <div class="form-group text-center">
+            <button class="addProductButton" type="submit" name="action" value="verzenden">Aanbieden</button>
+        </div>
     </form>
 </main>
