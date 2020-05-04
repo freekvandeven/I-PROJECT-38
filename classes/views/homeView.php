7f3b924e88de101aff346ab83b69e98b83d96e87
joonsv <joonsv@gmail.com>
2020-05-04 23:30:23 +0200

    add catalogusView.php

--- classes/views/homeView.php
+++ classes/views/homeView.php
@@ -70,6 +70,7 @@
                     </a>
                     <div class='card-body'>
                         <h5 class='card-title'><?=$card['Titel']?></h5>
+                        <p class="card-text">	&euro; <?= $card['prijs']?></p>
                         <p class='card-text'><?=$card['Beschrijving']?></p>
                         <a href='item.php?id=<?=$card['Voorwerpnummer']?>' class='card-link'>Meer informatie</a>
                     </div>
