6f8837dfb264bad31bf4d60443f6f496cd04a1ed
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-05 17:42:08 +0200

    Meer overzicht in code

--- includes/footer.php
+++ includes/footer.php
@@ -2,89 +2,59 @@
         <footer>
             <div class="bovenstukFooter">
                 <div class="container">
-
-                    <!-- Grid row-->
                     <div class="row py-4 d-flex align-items-center">
 
-                        <!-- Grid column -->
+                        <!-- Tekst boven de footer -->
                         <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
                             <h5 class="mb-0">Volg ons nu ook op social media!</h5>
                         </div>
 
-                        <!-- Grid column -->
+                        <!-- Social media links -->
                         <div class="col-md-6 col-lg-7 text-center text-md-right">
                             <!-- Facebook -->
-                            <a href="#">
-                                <img src="images/footerimages/facebook.png" alt="Facebook logo">
-                            </a>
+                            <a href="#"><img src="images/footerimages/facebook.png" alt="Facebook logo"></a>
                             <!-- Twitter -->
-                            <a href="#">
-                                <img src="images/footerimages/twitter.png" alt="Twitter logo">
-                            </a>
+                            <a href="#"><img src="images/footerimages/twitter.png" alt="Twitter logo"></a>
                             <!-- Instagram -->
-                            <a href="#">
-                                <img src="images/footerimages/instagram.png" alt="Instagram logo">
-                            </a>
+                            <a href="#"><img src="images/footerimages/instagram.png" alt="Instagram logo"></a>
                         </div>
-                        <!-- Grid column -->
+
                     </div>
-                    <!-- Grid row-->
                 </div>
             </div>
 
             <!-- Footer Links -->
             <div class="container text-center text-md-left mt-5">
-                <!-- Grid row -->
                 <div class="row mt-3">
-                    <!-- Grid column -->
+
+                    <!-- 1e kolom -->
                     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                        <!-- Content -->
                         <h5>EenmaalAndermaal</h5>
                         <hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto">
                         <p>Bij EenmaalAndermaal is de klant koning. Daarom kan de klant zijn eigen producten aanbieden en op die van een ander bieden.</p>
                     </div>
-                    <!-- Grid column -->
+                    <!-- 2e kolom -->
                     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                        <!-- Links -->
                         <h5>Handige links</h5>
                         <hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto">
-                        <p>
-                            <a href="profile.php">Jouw account</a>
-                        </p>
-                        <p>
-                            <a href="#">Veilingen bekijken</a>
-                        </p>
-                        <p>
-                            <a href="#">Product verkopen</a>
-                        </p>
-                        <p>
-                            <a href="#">Help</a>
-                        </p>
+                        <p><a href="profile.php">Jouw account</a></p>
+                        <p><a href="#">Veilingen bekijken</a></p>
+                        <p><a href="#">Product verkopen</a></p>
+                        <p><a href="#">Help</a></p>
                     </div>
 
-                    <!-- Grid column -->
+                    <!-- 3e kolom -->
                     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                        <!-- Links -->
                         <h5>Over ons</h5>
                         <hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto">
-                        <p>
-                            <a href="#">Projecten</a>
-                        </p>
-                        <p>
-                            <a href="#">Verdienmodel</a>
-                        </p>
-                        <p>
-                            <a href="#">Adverteren</a>
-                        </p>
-                        <p>
-                            <a href="#">Vacatures</a>
-                        </p>
+                        <p><a href="#">Projecten</a></p>
+                        <p><a href="#">Verdienmodel</a></p>
+                        <p><a href="#">Adverteren</a></p>
+                        <p><a href="#">Vacatures</a></p>
                     </div>
 
-                    <!-- Grid column -->
+                    <!-- 4e kolom -->
                     <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                        <!-- Contactgegevens -->
                         <h5>Contact</h5>
                         <hr class="accent-2 mb-4 mt-0 d-inline-block mx-auto">
                         <p><img src="images/footerimages/plaats.png" alt="Home image">8th Ave, New York, New York</p>
@@ -92,14 +62,16 @@
                         <p><a href="tel:+31 6 123456"><img src="images/footerimages/telefoon.png" alt="Phone image">+31 6 123456</a></p>
                         <p><a href="tel:0612345678"><img src="images/footerimages/mobiel.png" alt="Phone image">06 12345678</a></p>
                     </div>
+
                 </div>
-            </div>
             <!-- Einde Footer Links -->
+            </div>
 
             <!-- Copyright -->
             <div class="footer-copyright text-center py-3">
-                © 2020 Copyright: EenmaalAndermaal
+                &copy; 2020 Copyright: EenmaalAndermaal
             </div>
+
         </footer>
     </body>
 </html>
