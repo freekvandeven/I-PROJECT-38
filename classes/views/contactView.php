1140f0a109e06f10313aa52ea77b15363f35fc9e
Amos932 <amosmiddelkoop2001@gmail.com>
2020-05-05 10:21:24 +0200

    Contactform with handling

--- classes/views/contactView.php
+++ classes/views/contactView.php
@@ -4,4 +4,20 @@
         <h2 class="display-5">Welkom op EenmaalAndermaal</h2>
         <p class="lead">EenmaalAndermaal is een online veilingsite waar u uw producten kunt aanbieden en kunt bieden op andermans producten.</p>
     </div>
-</main>
+
+    <h1>Contact</h1>
+    <p>Heeft u vragen? Neem dan gerust met ons contact op!: </p>
+    <form class="formulier" method="POST" action="">
+        <h2>Formulier</h2><br>
+        <label for="voornaam">Voornaam</label>
+        <input type="text" name="first-name" id="voornaam" ><br>
+        <label for="achternaam">Voornaam</label>
+        <input type="text" name="surname" id="achternaam" ><br>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" > <br>
+        <label for="telefoon"> Telefoonnummer</label>
+        <input type="tel" name="phone-number" id="telefoon" ><br>
+        <label for="bericht">Bericht</label>
+        <textarea name="message" id="bericht"></textarea> <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
--- contact.php
+++ contact.php
@@ -2,9 +2,14 @@
 session_start();
 
 $title = "Contact Page";
+
+if(!empty($_POST) && isset($_POST['first-name'],$_POST['surname'],$_POST['phone-number'], $_POST['message'])){
+    Database :: getFeedback($_POST['first-name'],$_POST['surname'],$_POST['phone-number'], $_POST['message']);
+}
+
 require_once('includes/functions.php');
 
-require_once('includes/header.php');
+require_once 'includes/header.php';
 
 require_once('classes/views/contactView.php');
 
