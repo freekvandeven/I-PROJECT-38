66fb6dedf25b42dbaf4128dea309f6d183b45f86
TimvR1999 <tch.vanroermund@student.han.nl>
2020-05-05 10:35:26 +0200

    item page for a single item

---
+++ classes/views/itemView.php
@@ -0,0 +1,44 @@
+<main>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-md-4 mt-2">
+                <form>
+                    <div class="form-group text-left">
+                        <label for="inputBod">Bieden</label>
+                        <input type="bod" class="form-control" id="inputBod" placeholder="&euro;">
+                    </div>
+                    <button type="submit" class="btn btn-outline-secondary">Plaats bod</button>
+                </form>
+                <ul class="list-group mt-4">
+                    <li class="list-group-item"><strong>Ruurd</strong> &euro; 60</li>
+                    <li class="list-group-item"><strong>Sanne</strong> &euro; 56</li>
+                </ul>
+            </div>
+            <div class="col-xl-6 col-md-4 mt-2">
+                <div class='card'>
+                    <div class='card-body'>
+                        <h4 class="card-title">Houten kast</h4>
+                        <img src='upload/items/21.jpg' class='card-img-top' alt='Productnaam'>
+                        <p class='card-text'><?= $card['Beschrijving'] ?></p>
+                    </div>
+                    <div class='card-footer'>
+                        <small class='text-muted'>Nog: 4min beschikbaar.</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-4 mt-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Verkoper</h4>
+                        <img src="images/profilePicture.png" class="card-img" alt="profielfoto">
+                        <p><b>Voornaam: </b><?= $profile_data['Voornaam'] ?></p>
+                        <p><b>Achternaam: </b><?= $profile_data['Achternaam'] ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
---
+++ item.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once('includes/functions.php');
+
+
+$title = "Item pagina";
+require_once('includes/header.php');
+
+require_once('classes/views/itemView.php');
+
+require_once('includes/footer.php');
