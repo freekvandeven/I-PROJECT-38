dd1eba3bfeb1b780e02513853297240032a91c12
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-07 19:07:10 +0200

    Added adminpage

--- classes/views/adminView.php
+++ classes/views/adminView.php
@@ -1,13 +1,98 @@
-<main>
-    <h2>Welkom master op de Admin pagina</h2>
-    <ul>
-        <li><a href="admin.php?category=auction">Bekijk veilingen</a></li>
-        <li><a href="admin.php?category=user">Bekijk gebruikers</a></li>
-        <li><a href="admin.php?category=seller">Bekijk verkopers</a></li>
-        <li><a href="admin.php?category=statistics">Bekijk pagina statistieken</a></li>
-        <li><a href="admin.php?category=reset">Reset Database</a></li>
-        <li><a href="admin.php?category=fill">Vul database met CSV bestand</a></li>
-        <li><a href="admin.php?category=query">MS SQL Server Query Editor</a></li>
-        <li><a href="admin.php?category=addRubriek">Voeg een Rubriek toe</a></li>
-    </ul>
+<main class="adminPagina">
+    <div class="jumbotron">
+        <h2 class="display-5">Welkom master op de Adminpagina</h2>
+        <p>Op deze pagina heeft u een aantal opties waar alleen u toegang tot heeft.</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <a href="admin.php?category=auction">
+                    <div class="card">
+                        <h4 class="card-title text-center">Bekijk alle veilingen</h4>
+                        <div class="card-body">
+                            Appel
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+<!--    <ul>-->
+<!--        <li><a href="admin.php?category=auction">Bekijk veilingen</a></li>-->
+<!--        <li><a href="admin.php?category=user">Bekijk gebruikers</a></li>-->
+<!--        <li><a href="admin.php?category=seller">Bekijk verkopers</a></li>-->
+<!--        <li><a href="admin.php?category=statistics">Bekijk pagina statistieken</a></li>-->
+<!--        <li><a href="admin.php?category=reset">Reset Database</a></li>-->
+<!--        <li><a href="admin.php?category=fill">Vul database met CSV bestand</a></li>-->
+<!--        <li><a href="admin.php?category=query">MS SQL Server Query Editor</a></li>-->
+<!--        <li><a href="admin.php?category=addRubriek">Voeg een Rubriek toe</a></li>-->
+<!--    </ul>-->
 </main>
