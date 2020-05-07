cafe81838a15501f1da56f181f8ce1c19a282d5b
Amos932 <amosmiddelkoop2001@gmail.com>
2020-05-07 16:40:03 +0200

    Improving Admin Page

--- classes/views/adminView.php
+++ classes/views/adminView.php
@@ -1,12 +1,16 @@
 <main>
-    <h2>Welkom master op de Admin pagina</h2>
-    <ul>
-        <li><a href="admin.php?category=auction">Bekijk veilingen</a></li>
-        <li><a href="admin.php?category=user">Bekijk gebruikers</a></li>
-        <li><a href="admin.php?category=seller">Bekijk verkopers</a></li>
-        <li><a href="admin.php?category=statistics">Bekijk pagina statistieken</a></li>
-        <li><a href="admin.php?category=reset">Reset Database</a></li>
-        <li><a href="admin.php?category=fill">Vul database met CSV bestand</a></li>
-        <li><a href="admin.php?category=query">MS SQL Server Query Editor</a></li>
-    </ul>
+    <div class="admin-text">
+        <h1>Welkom master op de Admin pagina</h1>
+        <h2> Door deze pagina is het mogelijk om inzicht te krijgen in veiligen, gebruikers en pagina statistieken!</h2>
+        <h2>Hier kunt u verschillende factoren bekijken:</h2>
+        <button class="inlogButton"formaction="admin.php?category=auction" >Bekijk veilingen</button>
+        <button class="inlogButton"formaction="admin.php?category=user" >Bekijk gebruikers</button>
+        <button class="inlogButton" formaction="admin.php?category=seller" >Bekijk verkopers</button>
+        <button class="inlogButton" formaction="admin.php?category=statistics">Bekijk pagina statistieken</button>
+        <h2>Hier kunt u zich bezighouden met database gerelateerde werkzaamheden:</h2>
+        <button class="inlogButton" formaction="admin.php?category=reset" >Reset database</button>
+        <button class="inlogButton" formaction="admin.php?category=fill" >Vul database met CSV bestand</button>
+        <h2>Hier is het mogelijk om een nieuwe rubriek toe te voegen:</h2>
+        <button class="inlogButton" formaction="admin.php?category=query" >MS SQL Server Query Editor</button>
+    </div>
 </main>
