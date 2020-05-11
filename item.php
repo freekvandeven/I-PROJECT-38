58f19da50e04064aaac4ecc7149111b3b5cbe20c
Amos932 <amosmiddelkoop2001@gmail.com>
2020-05-11 14:44:54 +0200

    Minimum increase offer item

--- item.php
+++ item.php
@@ -4,10 +4,37 @@ require_once('includes/functions.php');
 if(checkPost()){
     checkLogin();
     $ref = $_POST['voorwerp'];
+    // Validatie verzending bod
     if(isset($_POST["bid"]) && !empty($_POST["bid"])){
-        if($_POST["bid"] > Items::getHighestBid($ref)['Bodbedrag'] && $_POST["bid"] > Items::getItem($ref)["Startprijs"]){
-            Items::placeBid($ref, $_POST["bid"], $_SESSION['name']);
+        // initialisatie variabelen
+        $highestBid = Items::getHighestBid($ref)['Bodbedrag'];
+        $startPrijs = Items::getItem($ref)["Startprijs"];
+
+        // bepalen van startprijs
+        if($startPrijs >= 10 && $startPrijs <= 25){
+            $minimumIncrease = ($highestBid / 100) * 0.028;
+        } else if ($startPrijs > 25 && $startPrijs <= 50){
+            $minimumIncrease = ($highestBid / 100) * 0.026;
+        } else if ($startPrijs > 50 && $startPrijs <= 75){
+            $minimumIncrease = ($highestBid / 100) * 0.025;
+        } else if($startPrijs > 75 && $startPrijs <= 100){
+            $minimumIncrease = ($highestBid / 100) * 0.021;
+        } else if($startPrijs > 100 && $startPrijs < 250){
+            $minimumIncrease = ($highestBid / 100) * 0.018;
+        } else if($startPrijs > 250 && $startPrijs < 500){
+            $minimumIncrease = ($highestBid / 100) * 0.013;
+        } else if($startPrijs > 500 && $startPrijs < 750){
+            $minimumIncrease = ($highestBid / 100) * 0.01;
+        } else if($startPrijs > 750){
+            $minimumIncrease = ($highestBid / 100) * 0.005;
         }
+
+        if($_POST["bid"] > $highestBid && $_POST["bid"] > $startPrijs){
+            if( ($_POST["bid"] - $highestBid) > $minimumIncrease){
+                Items::placeBid($ref, $_POST["bid"], $_SESSION['name']);
+            }
+        }
+
     } else {
         $err = "bid is to low";
     }
