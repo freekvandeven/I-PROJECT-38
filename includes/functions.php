67cd71e97c0680abbd345712a1e8a31e1c5fcd04
Freek van de Ven <freek@freekvandeven.nl>
2020-05-18 11:40:33 +0200

    add distance calculation

--- includes/functions.php
+++ includes/functions.php
@@ -114,7 +114,52 @@ function storeImg($files, $id,$target_dir)
 {
     move_uploaded_file($files, $target_dir . $id .'.png');
 }
- 
+
+function calculateDistance($point1, $point2, $unit = ''){
+    $apiKey = 'AIzaSyBt6UzzpaNgxMJPT62WvvWp5Q7DKuR9GL8';
+    //$apiKey = 'AIzaSyBA5t_6kDT86NEzXrXQSzcaZpKLbDRzBos';
+    $formattedAddrFrom = str_replace(' ', '+', $point1);
+    $formattedAddrTo = str_replace(' ', '+', $point2);
+
+    // Geocoding API request with start address
+    $geocodeFrom = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddrFrom.'&sensor=false&key='.$apiKey);
+    $outputFrom = json_decode($geocodeFrom);
+    if(!empty($outputFrom->error_message)){
+        return $outputFrom->error_message;
+    }
+
+    // Geocoding API request with end address
+    $geocodeTo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddrTo.'&sensor=false&key='.$apiKey);
+    $outputTo = json_decode($geocodeTo);
+    if(!empty($outputTo->error_message)){
+        return $outputTo->error_message;
+    }
+
+    // Get latitude and longitude from the geodata
+    $latitudeFrom    = $outputFrom->results[0]->geometry->location->lat;
+    $longitudeFrom    = $outputFrom->results[0]->geometry->location->lng;
+    $latitudeTo        = $outputTo->results[0]->geometry->location->lat;
+    $longitudeTo    = $outputTo->results[0]->geometry->location->lng;
+
+    // Calculate distance between latitude and longitude
+    $theta    = $longitudeFrom - $longitudeTo;
+    $dist    = sin(deg2rad($latitudeFrom)) * sin(deg2rad($latitudeTo)) +  cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * cos(deg2rad($theta));
+    $dist    = acos($dist);
+    $dist    = rad2deg($dist);
+    $miles    = $dist * 60 * 1.1515;
+
+    // Convert unit and return distance
+    $unit = strtoupper($unit);
+    if($unit == "K"){
+        return round($miles * 1.609344, 2).' km';
+    }elseif($unit == "M"){
+        return round($miles * 1609.344, 2).' meters';
+    }else{
+        return round($miles, 2).' miles';
+    }
+
+}
+
 function sendConfirmationEmail($mail, $username, $hash){
     $subject = "Bevestig je account";
     $variables = [];
