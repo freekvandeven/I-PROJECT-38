3bfd4a81bba4e71e3e33c24ba1ad9f1f7d3ff88c
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-18 18:01:00 +0200

    meerdere productfoto's

--- addProduct.php
+++ addProduct.php
@@ -5,6 +5,8 @@ checkLogin();
 $parameterList = array("Titel", "Beschrijving", "Rubriek", "img", "Startprijs", "Betalingswijze", "Betalingsinstructie", "Looptijd",
     "Verzendinstructies");
 $user = User::getUser($_SESSION["name"]);
+$maxOptionalPhotos = 5;
+
 if ($user["Verkoper"]) {
     if(checkPost()) {
         if (isset($_POST["Titel"]) && isset($_POST["Beschrijving"]) && isset($_POST["Startprijs"]) && isset($_FILES["img"]) && isset($_POST["Verzendinstructies"])) {
@@ -23,22 +25,53 @@ if ($user["Verkoper"]) {
                 ":LooptijdEindeDag" => date('Y-m-d', strtotime($date . ' + ' . $_POST['Looptijd'] . ' days')),
                 ":LooptijdEindeTijdstip" => date("H:i:s"), ":VeilingGesloten" => "Nee", ":Verkoopprijs" => $posts["Startprijs"]);
             //if image is set
-            if(isset($_FILES)){
+            if(isset($_FILES['thumbnail'])){
                 //try insert
                 if(Items::insertItem($item)) {
                     $itemId= Items::get_ItemId();
                     Items::insertIntoRubriek($itemId,$_POST['Rubriek']);
                     // if not png
-                    if ($_FILES['img']['type'] != 'image/png') {
+                    if ($_FILES['thumbnail']['type'] != 'image/png') {
                         //convert to png
-                        imagepng(imagecreatefromstring(file_get_contents($_FILES['img']['tmp_name'])), 'upload/items/tempItem.png');
+                        imagepng(imagecreatefromstring(file_get_contents($_FILES['thumbnail']['tmp_name'])), 'upload/items/tempItem.png');
                     }
-                    //store file with new autoincrementId as id.png
-                    storeImg($_FILES['img']['tmp_name'], $itemId,"upload/items/");
-                    header("Location: profile.php"); // send person to his item page
+
+                    $insertFilesArray['Filenaam'] = $itemId . png;
+                    $insertFilesArray['Voorwerp'] = $itemId;
+                    if(Items::insertFiles($insertFilesArray)) {
+                        //store file with new autoincrementId as id.png
+                        storeImg($_FILES['thumbnail']['tmp_name'], $itemId,"upload/items/");
+                    } else {
+                        $err = "Er ging iets mis met de database.";
+                    }
+
+                    //If there are any optional photos added
+                    if(isset($_FILES['img'])) {
+                        // re-organizes array to fill the data easier
+                        $optionalPhotosArray = reOrganizeArray($_FILES['img']);
+                        // per photo
+                        for($i=0; $i<count($optionalPhotosArray) && $i < $maxOptionalPhotos; $i++) {
+                            // converts to .png
+                            if ($optionalPhotosArray[$i]['type'] != 'image/png') {
+                                //convert to png
+                                imagepng(imagecreatefromstring(file_get_contents($_FILES['img']['tmp_name'])), 'upload/items/tempItem.png');
+                            }
+                            $insertFilesArray['Filenaam'] = $itemId . _ . $i . png;
+                            $insertFilesArray['Voorwerp'] = $itemId;
+                            if (Items::insertFiles($insertFilesArray)) {
+                                //stores file with new autoincrementId + _$i as id_$i.png
+                                storeImg($optionalPhotosArray[$i]['tmp_name'], $itemId . _ . $i, "upload/items/");
+                                header("Location: profile.php");
+                            } else {
+                                $err = "Er ging iets mis met de database.";
+                            }
+                        }
+                    }
+                } else{
+                    $err = "Er ging iets mis met de database.";
                 }
-            }else{
-                $err = "something went wrong";
+            } else{
+                $err = "Een thumbnail is verplicht!";
             }
         } else {
             $err = "please fill in all the data!";
