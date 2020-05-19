aee014386f09be07efbfbda3fb2e346b398ae427
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-19 11:22:00 +0200

    legenda's in mijnveilingen en mijnfavorieten

--- classes/views/profile/itemView.php
+++ classes/views/profile/itemView.php
@@ -3,28 +3,68 @@
     $displayedItems = array("Titel", "Startprijs", "Betalingswijze", "Betalingsinstructie", "Plaatsnaam", "Land", "Looptijd", "VeilingGesloten");
 ?>
 
-<main>
-    <div class="mijnVeilingenBox">
-        <h2 class="text-center">Mijn veilingen</h2>
-        <h5 class="text-center">Al uw veilingen kunt u hieronder zien.</h5>
+<main class="mijnVeilingenPagina">
+    <h2 class="text-center">Mijn veilingen</h2>
+    <h5 class="text-center">Als u een veiling aanbiedt, kunt u die hieronder volgen.</h5>
+
+    <div class="tabel col-xl-10 offset-xl-1">
+        <div class="legendaButton text-right">
+            <button type="button" class="legendaDropdownButton dropdown-toggle" data-toggle="dropdown">Legenda</button>
+            <div class="dropdown-menu dropdown-menu-right">
+                <div class="dropdown-item">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Kleur en betekenis</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="table-success">
+                            <td>Veiling is afgelopen en er is WEL een bieder.</td>
+                        </tr>
+
+                        <tr class="table-danger">
+                            <td>Veiling is afgelopen en er is GEEN bieder.</td>
+                        </tr>
+
+                        <tr class="table-orange">
+                            <td>Veiling is bezig, maar er is nog geen bieder.</td>
+                        </tr>
+
+                        <tr class="table-info">
+                            <td>Veiling is bezig en er is al een bieder.</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
         <div class="table-responsive">
             <table class="table table-hover">
                 <thead>
-                    <tr>
-                        <th>Link</th>
-                        <th>Koper</th>
-                        <?php foreach($displayedItems as $key){
-                            echo "<th>$key</th>";
-                        } ?>
-                    </tr>
+                <tr>
+                    <th>Link</th>
+                    <th>Koper</th>
+                    <?php foreach($displayedItems as $key){
+                        echo "<th>$key</th>";
+                    } ?>
+                </tr>
                 </thead>
                 <tbody>
                 <?php foreach($items as $item):
                     if($item['VeilingGesloten'] == "Wel "){
-                        $label = "table-success";
+                        if(isset($item['Koper'])) {
+                            $label = "table-success";
+                        } else {
+                            $label = "table-danger";
+                        }
                     } else {
-                        $label = "table-danger";
+                        if(isset($item['Koper'])) {
+                            $label = "table-info";
+                        } else{
+                            $label = "table-orange";
+                        }
                     }
                     ?>
                     <tr class="<?=$label?>">
@@ -38,34 +78,9 @@
                 </tbody>
             </table>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="legenda col-xl-3 offset-xl-9 col-lg-5 offset-lg-7 col-md-5 offset-md-7 col-sm-12 col-12 ">
-                <h5 class="text-center">Legenda</h5>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Kleur en betekenis</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="table-success">
-                                <td>Veiling is afgelopen.</td>
-                            </tr>
-
-                            <tr class="table-danger">
-                                <td>Veiling is nog niet afgelopen.</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-            <div class="form-group text-center col-lg-12">
-                <p class="gaTerugKnop" style="margin-bottom: 60px;"><a href="profile.php">Ga terug</a></p>
-            </div>
-
-        </div>
+    <div class="form-group text-center">
+        <p class="gaTerugKnop"><a href="profile.php">Ga terug</a></p>
     </div>
 </main>
