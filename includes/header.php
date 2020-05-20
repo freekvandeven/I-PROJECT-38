06f17d3800e808271fb11e8030816a801b594679
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-21 00:59:49 +0200

    fixed extra stuffies

--- includes/header.php
+++ includes/header.php
@@ -55,7 +55,7 @@ if(isset($_SESSION['loggedin'])){
     </script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="index.php">EenmaalAndermaal</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
