e6c7a9cdc30bab3a868f93c17e96622b1e488dbd
Anthonyvago <40485941+Anthonyvago@users.noreply.github.com>
2020-05-20 13:26:37 +0200

    Darkmode addProductView.php

--- includes/darkMode.js
+++ includes/darkMode.js
@@ -1,10 +1,10 @@
 var options = {
-    bottom: '32px', // default: '32px'
+    bottom: '85%', // default: '32px'
     right: '32px', // default: '32px'
     left: 'unset', // default: 'unset'
-    time: '1s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
+    time: '0.3s', // default: '0.3s'
+    mixColor: 'rgb(180, 180, 180)', // default: '#fff'
+    backgroundColor: 'rgb(255, 255, 255)',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     saveInCookies: true, // default: true,
