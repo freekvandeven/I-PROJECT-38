9cc645c64822f6f1a09bea0424c0afaa2111c168
Freek van de Ven <freek@freekvandeven.nl>
2020-05-27 17:36:46 +0200

    small fix

--- classes/controllers/admin/resetController.php
+++ classes/controllers/admin/resetController.php
@@ -3,5 +3,5 @@
 # this controller file gets run when the admin wants to reset the page
 
 setupDatabase();
-
+$toast = 'database reset succesfully completed';
 $_GET['category'] = $_POST['category'];
--- includes/more/admin.php
+++ includes/more/admin.php
@@ -7,15 +7,18 @@ function setupDatabase() // setup the database
 
     $delete = file_get_contents('includes/SQL/clearDatabase.sql');
     $data = $dbh->exec($delete);
-    if ($serverType != "mysql") {
+    if ($serverType != "mysql") { // distinguish between sqlsrv and MySQL
         $create = file_get_contents('includes/SQL/setupSQLsrv.sql');
     } else {
         $create = file_get_contents('includes/SQL/setupMySQL.sql');
     }
     $data = $dbh->exec($create);
-    // distinguish between sqlsrv and MySQL
-    $sql = file_get_contents('includes/SQL/constraints.sql');
-    $data = $dbh->exec($sql);
+    $constraints = file_get_contents('includes/SQL/constraints.sql');
+    $data = $dbh->exec($constraints);
+
+    $creation = file_get_contents('includes/SQL/defaultData.sql');
+    $data = $dbh->exec($creation);
+
     $toast = 'database reset completed';
 }
 
