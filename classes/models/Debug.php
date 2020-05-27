c5612f838357684baed52e5825824fafa7dc8ddf
joonsv <joonsv@gmail.com>
2020-05-27 16:31:54 +0200

    added msg to debug::dump for eas of recognition

--- classes/models/Debug.php
+++ classes/models/Debug.php
@@ -1,6 +1,7 @@
 <?php
 class Debug{
-    static function dump($input){
+    static function dump($input,$msg = null){
+        echo "$msg";
         echo '<pre>' , var_dump($input) , '</pre>';
     }
 
