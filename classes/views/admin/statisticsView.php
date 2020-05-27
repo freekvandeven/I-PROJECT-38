7a39ec7b9e7ccffb5bdd47f7fd028e736f40135e
Amos932 <amosmiddelkoop2001@gmail.com>
2020-05-27 22:54:56 +0200

    Website statistics for each page on the site

--- classes/views/admin/statisticsView.php
+++ classes/views/admin/statisticsView.php
@@ -35,6 +35,87 @@
         </div>
     </div>
 
+    <div class="container">
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script>
+        let myChart = document.getElementById('myChart').getContext('2d');
+
+        // Global Options
+        Chart.defaults.global.defaultFontFamily = 'Lato';
+        Chart.defaults.global.defaultFontSize = 17;
+        Chart.defaults.global.defaultFontColor = '#777';
+
+        let massPopChart = new Chart(myChart, {
+            type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+            data:{
+                labels:['404', 'addProduct', 'admin', 'catalogus', 'contact', 'footer', 'forgotPassword', 'getCatalogus', 'index', 'item', 'loging', 'profile'],
+                datasets:[{
+                    label:'Site Activity',
+                    data:[
+                        117594,
+                        181045,
+                        153060,
+                        106519,
+                        105162,
+                        95072,
+                        117594,
+                        181045,
+                        253060,
+                        106519,
+                        105162,
+                        95072
+                    ],
+                    //backgroundColor:'green',
+                    backgroundColor:[
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(150, 99, 132, 0.6)',
+                        'rgba(255, 33, 90, 0.6)',
+                        'rgba(200, 196, 86, 0.6)',
+                        'rgba(75, 192, 170, 11)',
+                        'rgba(255, 200, 30, 200)',
+                        'rgba(20, 250, 86, 131)',
+                    ],
+                    borderWidth:1,
+                    borderColor:'#777',
+                    hoverBorderWidth:3,
+                    hoverBorderColor:'#000'
+                }]
+            },
+            options:{
+                title:{
+                    display:true,
+                    text:'Website statistieken van EenmaalAndermaal',
+                    fontSize:25
+                },
+                legend:{
+                    display:true,
+                    position:'right',
+                    labels:{
+                        fontColor:'#000'
+                    }
+                },
+                layout:{
+                    padding:{
+                        left:0,
+                        right:0,
+                        bottom:0,
+                        top:0
+                    }
+                },
+                tooltips:{
+                    enabled:true
+                }
+            }
+        });
+    </script>
+
     <div class="gaTerugKnopBox text-center">
         <a class="gaTerugKnop" href="admin.php">Ga terug</a>
     </div>
