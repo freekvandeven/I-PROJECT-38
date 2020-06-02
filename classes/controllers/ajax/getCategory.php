4b537489cf77e38589fdd81a608d0a58285f2ce1
Freek van de Ven <freek@freekvandeven.nl>
2020-06-02 13:45:23 +0200

    improve rubrieken filter

--- classes/controllers/ajax/getCategory.php
+++ classes/controllers/ajax/getCategory.php
@@ -2,7 +2,7 @@
 $search = '%' . $_POST["search"] . '%';
 $hint = "";
 // get the corresponding categories
-$sql = "SELECT Rubrieknaam FROM Rubriek WHERE Rubrieknaam LIKE :search";
+$sql = "SELECT TOP 50 Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Rubrieknaam LIKE :search";
 $data = $dbh->prepare($sql);
 $data->bindValue(':search', $search, PDO::PARAM_STR);
 $data->execute();
@@ -11,7 +11,7 @@ $result = $data->fetchAll(PDO::FETCH_ASSOC);
 
 // loop over the categorie
 foreach($result as $category){
-    $hint.= "<li>" . $category['Rubrieknaam'] . "</li>";
+    $hint.= "<li class='" . $category['Rubrieknummer'] . "'>" . $category['Rubrieknaam'] . "</li>";
 }
 // echo "<li> Category</li>";
 //$hint = "<li>Test1</li><li>Test2</li><li>Test4</li>";
