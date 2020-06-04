13b5faaa689eba23b70703a728d39525a6819f49
Freek van de Ven <freek@freekvandeven.nl>
2020-06-04 14:51:07 +0200

    add notification settings check

--- classes/controllers/ajax/getNotifications.php
+++ classes/controllers/ajax/getNotifications.php
@@ -1,8 +1,10 @@
 <?php
 // get amount of notifications
 if(isset($_SESSION['name'])) {
-    $notifications = User::getNotifications($_SESSION['name']);
-    if (!empty($notifications)) {
-        echo count($notifications);
+    if(User::getSettings($_SESSION['name'])['notifications']) {
+        $notifications = User::getNotifications($_SESSION['name']);
+        if (!empty($notifications)) {
+            echo count($notifications);
+        }
     }
 }
